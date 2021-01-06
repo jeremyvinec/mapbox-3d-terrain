9c408735dbbceeb78728a230be8387945653f91a
Jérémy Yvinec <jeremy.yvinec@gmail.com>
2021-01-06 10:55:53 +0100

    feat(App): setTerrain

--- src/App.js
+++ src/App.js
@@ -12,11 +12,34 @@ const App = () => {
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
-      // See style options here: https://docs.mapbox.com/api/maps/#styles
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-104.9876, 39.7405],
-      zoom: 12.5,
+      zoom: 13.1,
+      center: [6.0809918, 44.5558063],
+      pitch: 85,
+      bearing: 80,
+      style: 'mapbox://styles/mapbox-map-design/ckhqrf2tz0dt119ny6azh975y'
     });
+
+    map.on('load', function () {
+      map.addSource('mapbox-dem', {
+      'type': 'raster-dem',
+      'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
+      'tileSize': 512,
+      'maxzoom': 14
+      });
+      // add the DEM source as a terrain layer with exaggerated height
+      map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
+       
+      // add a sky layer that will show when the map is highly pitched
+      map.addLayer({
+      'id': 'sky',
+      'type': 'sky',
+      'paint': {
+      'sky-type': 'atmosphere',
+      'sky-atmosphere-sun': [0.0, 0.0],
+      'sky-atmosphere-sun-intensity': 15
+      }
+      });
+      });
 
     // add navigation control (the +/- zoom buttons)
     map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
